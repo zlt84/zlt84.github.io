5cb1342973bfb6b6a2ba565b9d9ddd6bce9f8ceb
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-02-06 17:14:48 +0100

    js - first steps

---
+++ scripts/20170206firststeps.js
@@ -0,0 +1,43 @@
+var myString = 'zlt';
+var myNumber = 14;
+var myBoolean = true;
+
+// one line comment
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+
+console.log(Math.floor(Math.random() * 100));
+
+/* commenting
+as long as you want
+really */
+
+// simple text
+console.log('Masters of War, torment every soul');
+console.log('Rape every whore that carries the cross');
+
+console.log('Fire!');
+console.log('Burn them all, burn them alive');
+console.log('Send their souls to Deathqueen’s hall');
+console.log('To the land of cold burning flames');
+console.log('Send them to the land of famine and despair');
+console.log('Eternally they will starve and freeze');
+
+
+var strength = '50,000 pounds';
+console.log('How much stuff can a variable hold? ', strength);
+
+var age = 32;
+console.log(age);
+var hasPet = true;
+console.log(hasPet);
+
+
+var morningAlarm = '6:30AM';
+morningAlarm = '7:00AM';
+console.log('Morning alarm is set to: ', morningAlarm);
+
+
+var favoriteAnimal = 'cat';
+console.log('My favorite animal: ' + favoriteAnimal);
