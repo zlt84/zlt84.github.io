81753d7b39087925a3b59a3d116882cf9a9ec8f5
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-02-10 14:15:21 +0100

    JS – alap tanfolyam a Codecademy-n megkezdve:)

---
+++ scripts/20170210.js
@@ -0,0 +1,30 @@
+confirm('Are you ready to DIE?')                //simple confirmation
+prompt('What is your last wish?')               //answerable question
+
+'the wanderers part is to ride alone'.length
+
+"I'm coding like a champ"                       //sima string
+"I'm coding like a champ".length                //a string hosszát adja meg, ami 23
+"I'm coding like a champ".length > 10           //megkérdezem, hogy igaz-e az állításom. a válasz: true
+
+console.log(2 * 5)                              //console.log-gal tudok kiírni a konzolra
+console.log('Hello')
+
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+
+
+var myName = 'Zsolt Katai'
+
+if ('myName'.length >= 15 ) {
+    console.log('You have a long name!');
+}
+
+else {
+    console.log('You have a short name!');
+}
