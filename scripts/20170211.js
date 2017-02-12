bfc1f07795ff86c11eddb7ef74756d10244089b1
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-02-12 11:30:31 +0100

    a new day will rise

--- scripts/20170211.js
+++ scripts/20170211.js
@@ -38,14 +38,139 @@ foodDemand('Alma')
 
 // Nicely written function:
 var calculate = function (number) {
-    var val = number * 10;
-    console.log(val);
+  var val = number * 10;
+  console.log(val);
 };
 
 // Badly written function with syntax errors!
 
 var greeting = function (name) {
-    console.log(name)
+  console.log(name)
 }
 
 greeting(name)
+
+
+function orangeCost(price) {
+  console.log(price * 5);
+}
+
+orangeCost(5)
+
+
+// Parameter is a number, and we do math with that parameter
+var timesTwo = function (number) {
+  return number * 2;
+};
+
+// Call timesTwo here!
+var newNumber = timesTwo(14)
+console.log(newNumber);
+
+
+var quarter = function (number) {
+  return number / 4;
+}
+
+if (quarter(12) % 3 === 0) {
+  console.log("The statement is true");
+} else {
+  console.log("The statement is false");
+}
+
+
+// Write your function starting on line 3
+var perimeterBox = function (length, width) {
+  return length + length + width + width;
+}
+
+perimeterBox(30, 40)
+
+
+var my_number = 7; //this has global scope
+
+var timesTwo = function (number) {
+  var my_number = number * 2;
+  console.log("Inside the function my_number is: ");
+  console.log(my_number);
+};
+
+timesTwo(7);
+
+console.log("Outside the function my_number is: ")
+console.log(my_number);
+
+
+var nameString = function (name) {
+  return 'Hi, I am' + ' ' + name
+};
+
+console.log(nameString('Zsolt'))
+
+
+// Ko papir ollo jatek:)
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+var computerChoice = Math.random();
+
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if (computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+  computerChoice = "scissors";
+}
+console.log("Computer: " + computerChoice);
+
+
+var compare = function (choice1, choice2) {
+  if (choice1 === choice2) {
+    return "The result is a tie!";
+  } else if (choice1 === "rock") {
+    if (choice2 === "scissors") {
+      return "rock wins"
+    } else {
+      "paper wins"
+    }
+  } else if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      return "paper wins"
+    } else {
+      "scissors wins"
+    }
+  }
+}
+
+compare()
+
+
+for (var i = 5; i <= 50; i += 5) {
+  console.log(i);
+}
+
+
+for (var i = 8; i < 120; i += 12) {
+  console.log(i);
+}
+
+
+for (var i = 10; i >= 0; i--) {
+  console.log(i);
+}
+
+for (var i = 100; i > 0; i -= 5) {
+  console.log(i)
+}
+
+
+var cities = ["Budapest", "Kemence", "Maglód", "Győr", "Szob"];
+
+for (var i = 0; i < cities.length; i++) {
+    console.log("I would like to visit " + cities[i]);
+}
+
+var names = ['Goku', 'Kuririn', 'Piccolo', 'Gohan', 'Vegeta']
+
+for (var i = 0; i < names.length; i++) {
+    console.log("I know someone called " + names[i]);
+}
