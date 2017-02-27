23327319f3ac653850d06b2a0fe2e42776dc035a
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-02-27 22:22:47 +0100

    objects and arrays

--- scripts/20170227.js
+++ scripts/20170227.js
@@ -55,17 +55,17 @@ switch (troll) {
 
 var user = prompt("What will be your nickname in this Adventure?").toUpperCase();
 
-switch(user) {
-    case "RAMY":
-        console.log("Welcome Lord Crystal!:)");
-        break;
-    case "OLD MAN":
-        console.log("Old Man arrived.");
-        break;
-    case "UNNAMEDPLAYER":
-        console.log("..from the shadows");
-        break;
-    default:
+switch (user) {
+  case "RAMY":
+    console.log("Welcome Lord Crystal!:)");
+    break;
+  case "OLD MAN":
+    console.log("Old Man arrived.");
+    break;
+  case "UNNAMEDPLAYER":
+    console.log("..from the shadows");
+    break;
+  default:
     console.log("I don’t know you!");
 }
 
@@ -73,11 +73,11 @@ var brave = prompt("Are you brave enough to fight the monster?").toUpperCase();
 var power = prompt("Do you have enough power to hit hard as hell?").toUpperCase();
 
 if (brave === "YES" && power === "YES") {
-    console.log("Great! Slay the beast immediately! *SLASH*SLASH*SLASH*");
-} else if (brave === "NO" || power ==="NO") {
-    console.log("It looks like you'll die in pain... Bravery is not enough to kill a monster like this... as only pure power is nothing without control. Say farewell for those who you loved.")
+  console.log("Great! Slay the beast immediately! *SLASH*SLASH*SLASH*");
+} else if (brave === "NO" || power === "NO") {
+  console.log("It looks like you'll die in pain... Bravery is not enough to kill a monster like this... as only pure power is nothing without control. Say farewell for those who you loved.")
 } else {
-    console.log("You are weak and coward, the worst combination ever. You’d better stay at home.")
+  console.log("You are weak and coward, the worst combination ever. You’d better stay at home.")
 }
 
 
@@ -90,5 +90,54 @@ console.log(elementThree)
 var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
 
 for (var i = 0; i < languages.length; i++) {
-    console.log(languages[i])
+  console.log(languages[i])
 }
+
+
+var me = {
+  name: 'Zsolt',
+  age: 33
+};
+
+var me2 = new Object();
+
+me2.name = "Zsolt";
+me2.age = 33;
+
+
+var object1 = {
+  name: 'objektumOne',
+  number: 1
+}
+
+
+var object2 = new Object();
+
+object2.name = "objektumTwo";
+object2.number = 2;
+
+
+var object3 = new Object();
+
+object3.name = "okjektumThree";
+object3.number = 3;
+
+
+var objectA = new Object();
+objectA.name = 'Zsolt';
+objectA.gender = 'male';
+
+
+var myArray = [84, true, 'warrior', objectA]
+
+
+var myObject = new Object();
+
+var newArray = [[14, 'Zsolt', 'Katai'], [myObject, true]]
+
+
+var myObject = {
+  name: 'Eduardo',
+  type: 'Most excellent',
+  interests: ['Sports', 'Psychedelic music and art', 'Nature']
+};
