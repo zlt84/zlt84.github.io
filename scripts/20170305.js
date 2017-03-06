86f2dc1443d38190c34f6a7bfd5d66602cb24e32
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-03-06 08:29:00 +0100

    0305

--- scripts/20170305.js
+++ scripts/20170305.js
@@ -46,3 +46,63 @@ square.calcArea = function () {
 
 var p = square.calcPerimeter();
 var a = square.calcArea();
+
+
+/*
+But this approach of adding in properties one at a time for every object is tedious! Instead of always using the boring Object constructor, we can make our own constructors.
+
+This way we can set the properties for an object right when it is created. So instead of using the Object constructor which is empty and has no properties, we can make our own constructors which have properties.
+*/
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
+
+
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(name, age, color) {
+  this.name = name;
+  this.age = age;
+  this.color = color;
+}
+
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+};
+
+var sally = new Person("Sally Bowles", 39);
+var holden = new Person("Holden Caulfield", 16);
+
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " + holden.age);
+
+
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function () {
+    return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter = function () {
+    return 2 * height + 2 * width;
+  };
+}
+
+var rex = new Rectangle(7, 3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
