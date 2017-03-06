676ddce9034ae990fb9da04e8c42bda1ad12b361
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-03-06 23:05:34 +0100

    late night session

--- scripts/20170306.js
+++ scripts/20170306.js
@@ -28,3 +28,42 @@ family[1] = new Person("bob", 42);
 family[2] = new Person("michelle", 8);
 // add the last family member, "timmy", who is 6 years old
 family[3] = new Person("timmy", 6);
+
+
+// Our Person constructor
+function Emberek(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Now we can make an array of people
+var family = new Array();
+family[0] = new Emberek("alice", 40);
+family[1] = new Emberek("bob", 42);
+family[2] = new Emberek("michelle", 8);
+family[3] = new Emberek("timmy", 6);
+
+// loop through our new array
+for (var i = 0; family.length >= i; i++) {
+  console.log(family[i]);
+}
+
+
+// Our person constructor
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function (person1, person2) {
+  return person1.age - person2.age;
+}
+
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+// get the difference in age between alice and billy using our function
+var diff = ageDifference(alice, billy)
+console.log(diff)
