ed742abedc596186ebff58ed24efc7c43c9c067e
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-03-07 21:22:51 +0100

    0307 movin' forward

--- scripts/20170306.js
+++ scripts/20170306.js
@@ -67,3 +67,55 @@ var billy = new Person("Billy", 25);
 // get the difference in age between alice and billy using our function
 var diff = ageDifference(alice, billy)
 console.log(diff)
+
+
+// Our person constructor
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function (person1, person2) {
+  return person1.age - person2.age;
+};
+
+// Make a new function, olderAge, to return the age of
+// the older of two people
+function olderAge(person1, person2) {
+  if (person1.age > person2.age) {
+    return person1.age;
+  } else {
+    return person2.age;
+  }
+}
+
+// Let's bring back alice and billy to test our new function
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+console.log("The older person is " + olderAge(alice, billy));
+
+
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+var spencer2 = new Object();
+spencer2.age = 22;
+spencer2.country = "United States";
+
+
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species;
+
+// use bracket notation for snoopy's age
+var age = snoopy["age"]
---
+++ scripts/20170307.js
@@ -0,0 +1,135 @@
+// 3 lines required to make harry_potter
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book(pages, author) {
+  this.pages = pages;
+  this.author = author;
+}
+
+// Use our new constructor to make the_hobbit in one line
+var the_hobbit = new Book(320, "J.R.R. Tolkien");
+
+
+/*
+Methods are like functions that are associated with a particular object.
+
+They are especially helpful when you want to either:
+
+Update the object properties
+Calculate something based on an object's properties.
+Here, we have included a Circle object, with a radius property representing the circle's radius. We have implemented an area function which calculates the circle's area. Notice we have used Math.PI to get the π value.
+*/
+
+function Circle(radius) {
+  this.radius = radius;
+  this.area = function () {
+    return Math.PI * this.radius * this.radius;
+
+  };
+  // define a perimeter method here
+  this.perimeter = function () {
+    return 2 * Math.PI * this.radius;
+  }
+};
+
+
+// Building an Address Book
+var bob = {
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-777",
+  email: "bob.jones@example.com"
+};
+
+console.log(bob.firstName);
+console.log(bob.lastName);
+console.log(bob.email);
+
+
+var bob = {
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-7777",
+  email: "bob.jones@example.com"
+};
+
+var mary = {
+  firstname: "Mary",
+  lastName: "Johnson",
+  phoneNumber: "(650) 888-8888",
+  email: "mary.johnson@example.com"
+}
+
+var contacts = [bob, mary]
+
+console.log(contacts[1].phoneNumber)
+
+
+var bob = {
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-7777",
+  email: "bob.jones@example.com"
+};
+
+var mary = {
+  firstName: "Mary",
+  lastName: "Johnson",
+  phoneNumber: "(650) 888-8888",
+  email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+// printPerson added here
+function printPerson(person) {
+  console.log(person.firstName + " " + person.lastName)
+}
+
+printPerson(contacts[0])
+printPerson(contacts[1])
+
+
+var bob = {
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-7777",
+  email: "bob.jones@example.com"
+};
+
+var mary = {
+  firstName: "Mary",
+  lastName: "Johnson",
+  phoneNumber: "(650) 888-8888",
+  email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+  console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+  var contactsLength = contacts.length;
+  for (var i = 0; i < contactsLength; i++) {
+    printPerson(contacts[i]);
+  }
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search(lastName) {
+  var contactsLength = contacts.length;
+  for (var i = 0; i < contactsLength; i++) {
+    if (contacts[i].lastName === "Jones") {
+      printPerson(contacts[i])
+    }
+  }
+}
+
+list();
+search();
