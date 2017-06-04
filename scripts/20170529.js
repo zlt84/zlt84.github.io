194150c0cf9e29b8f61c1febf86116fd347b9c63
Zsolt Kátai <zsolt.katai84@gmail.com>
2017-06-04 22:12:16 +0200

    0604 bena zsolt

--- scripts/20170529.js
+++ scripts/20170529.js
@@ -1,42 +1,58 @@
+"use strict";
+
 function rpickup(players) {
-    var playerCount = document.getElementById("playerCountSelector");
-    var playerCountFinal = playerCount.value;
-    console.log(playerCountFinal)
+  var playerCount = document.getElementById("playerCountSelector");
+  var playerCountFinal = playerCount.value;
+  console.log(playerCountFinal);
 
-    var playersMod = players.slice();
-    playersMod = shuffle(playersMod);
+  var playersMod = players.slice();
+  playersMod = shuffle(playersMod);
+  console.log(playersMod);
 
-    for (i = 0; i < playerCount * 2; i++) {
-        if (playerCount == 4) {
-            var teamA = [playersMod[0], playersMod[2]];
-            var teamB = [playersMod[1], playersMod[3]];
-        } else if (playerCount = 6) {
-            var teamA = [playersMod[0], playersMod[2], playersMod[4]];
-            var teamB = [playersMod[1], playersMod[3], playersMod[5]];
-            }
-            else {
-                var teamA = [playersMod[0], playersMod[2], playersMod[4], playersMod[6]];
-                var teamB = [playersMod[1], playersMod[3], playersMod[5], playersMod[7]];
-                }
-            }
+  if (playerCount === 4) {
+    var teamA = [playersMod[0], playersMod[2]];
+    var teamB = [playersMod[1], playersMod[3]];
 
-            var obj = {
-                teamA: teamA,
-                teamB: teamB
-            };
+    var obj = {
+      teamA: teamA,
+      teamB: teamB
+    };
 
-            return obj
-        }
+    return obj
 
-        function renderTeam(id, team) {
-            var elem = document.getElementById(id);
-            elem.innerHTML = team[0] + '<br>' + team[1]
-        }
+  } else if (playerCount === 6) {
+    var teamA = [playersMod[0], playersMod[2], playersMod[4]];
+    var teamB = [playersMod[1], playersMod[3], playersMod[5]];
 
-        function generateTeams() {
-            var teams = rpickup(players);
-            renderTeam('teamRed', teams.teamA);
-            renderTeam('teamBlue', teams.teamB)
-        }
+    var obj = {
+      teamA: teamA,
+      teamB: teamB
+    };
 
-        document.getElementById("generatorButton").addEventListener("click", generateTeams);
+    return obj
+
+  } else {
+    var teamA = [playersMod[0], playersMod[2], playersMod[4], playersMod[6]];
+    var teamB = [playersMod[1], playersMod[3], playersMod[5], playersMod[7]];
+
+    var obj = {
+      teamA: teamA,
+      teamB: teamB
+    };
+
+    return obj
+  }
+}
+
+function renderTeam(id, team) {
+  var elem = document.getElementById(id);
+  elem.innerHTML = team[0] + '<br>' + team[1]
+}
+
+function generateTeams() {
+  var teams = rpickup(players);
+  renderTeam('teamRed', teams.teamA);
+  renderTeam('teamBlue', teams.teamB)
+}
+
+document.getElementById("generatorButton").addEventListener("click", generateTeams);
